7f014d289884e83de823b39031e29c9763f31a95
KishalanPather <ki5halanp4ther@gmail.com>
2025-03-18 12:21:51 +0200

    updated versions

--- parse_bla.py
+++ parse_bla.py
@@ -4,34 +4,54 @@ import sys
 from lex_bla import tokens
 
 def p_program(p):
-    'Program : Statement'
-    p[0] = ('program', p[1], p[2], p[3])
+    '''Program : Statement Program 
+                | 
+    '''
+    p[0] = ('program', p[1], p[2])
 
 def p_statement(p):
-    'Statement : Identifier EQUALS Expression'
+    'Statement : ID EQUALS Expression'
     p[0] = ('statement',p[1], p[2], p[3])
 
 def p_expression(p):
     '''Expression : Expression A Term
                 | Expression S Term
-                | Term
     '''
     p[0] = ('expression', p[1], p[2], p[3])
+
+def p_expression_term(p):
+    '''Expression : Term
+    '''
+    p[0] = ('expressionTerm', p[1])
 
 def p_term(p):
     '''
     Term : Term M Factor
         | Term D Factor
-        | Factor
     '''
     p[0] = ('term',p[1], p[2], p[3])
+
+def p_term_factor(p):
+    '''Term : Factor
+    '''
+    p[0] = ('TermFactor', p[1])
 
 def p_factor(p):
     '''
     Factor : LEFT_BRACKET Expression RIGHT_BRACKET
-            | Identifier
     '''
     p[0] = ('factor', p[1], p[2], p[3])
+
+def p_factor_binary(p):
+    '''
+    Factor : BINARY_LITERAL
+    '''
+    p[0] = ('factorBinary-literal', p[1])
+
+def p_factor_id(p):
+    '''Factor : ID
+    '''
+    p[0] = ('FactorID', p[1])
 
 
 # Error rule for syntax errors
@@ -43,10 +63,19 @@ parser = yacc.yacc()
 
 while True:
    try:
-       s = "a=1"
-       pass
+       s = '''
+        // this is a comment
+        /* this is another comment */
+        /* this is one more
+        comment which should
+        be recognised */
+        a=1
+        '''
    except EOFError:
+       print("YEAH BIG BIG PROBLEM")
        break
    if not s: continue
    result = parser.parse(s)
    print(result)
+
+
